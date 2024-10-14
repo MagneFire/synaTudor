7a8d0c909d81fe7c96316e9c291951ee350e5a68
Darrel Griët <dgriet@gmail.com>
2024-10-14 21:17:44 +0200

    download different vendor driver
    Seems to work identically.
    
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

--- libtudor/download_driver.sh
+++ libtudor/download_driver.sh
@@ -5,6 +5,10 @@ TMP_DIR="$2"
 OUT_DIR="$3"
 DLLS=${@:4}
 
+echo $HASH_FILE
+echo $TMP_DIR
+echo $OUT_DIR
+
 mkdir -p "$TMP_DIR"
 
 #Download the driver executable and check hash
@@ -12,7 +16,8 @@ mkdir -p "$TMP_DIR"
 INSTALLER2="$TMP_DIR/installer2.exe"
 #wget https://ftp.hp.com/pub/softpaq/sp138001-138500/sp138227.exe -O "$INSTALLER1"
 #wget https://download.lenovo.com/consumer/mobiles/h4yf01af.exe -O "$INSTALLER2"
-wget https://download.lenovo.com/consumer/mobiles/im7f04af07wp.exe -O "$INSTALLER2"
+#wget https://download.lenovo.com/consumer/mobiles/im7f04af07wp.exe -O "$INSTALLER2"
+wget https://download.lenovo.com/consumer/mobiles/huy103af07m6.exe -O "$INSTALLER2"
 # shasum "$INSTALLER" | cut -d" " -f1 | cmp - "$HASH_FILE"
 
 #Extract the driver
@@ -25,5 +30,7 @@ innoextract -d "$WINDRV" "$INSTALLER2"
 mkdir -p "$OUT_DIR"
 for dll in $DLLS
 do
-    cp $(find "$WINDRV" -name "$dll") "$OUT_DIR/$dll"
+    f=$(find "$WINDRV" -name "$dll")
+    echo $f
+    cp $f "$OUT_DIR/$dll"
 done
